775ade162663afaed4581e20c5995d5252308f4c
2017398956 <2017398956@qq.com>
2025-08-01 18:14:33 +0800

    update

--- lib/pages/Page06.dart
+++ lib/pages/Page06.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/cupertino.dart';
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_app/base/ViewUtil.dart';
 
 class Page06 extends StatefulWidget {
@@ -8,14 +9,82 @@ class Page06 extends StatefulWidget {
   State<StatefulWidget> createState() {
     return Page06State();
   }
-
 }
 
 class Page06State extends State<Page06> {
+  @override
+  Widget build(BuildContext context) {
+    return ViewUtil.createPageView(context, "第六章 可滚动组件", InfiniteListView());
+  }
+}
+
+class InfiniteListView extends StatefulWidget {
+  const InfiniteListView({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _InfiniteListViewState();
+}
+
+class _InfiniteListViewState extends State<InfiniteListView> {
+  static const loadingTag = "##loading##"; //表尾标记
+  final _words = <String>[loadingTag];
+
+  @override
+  void initState() {
+    super.initState();
+    _retrieveData();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ViewUtil.createPageView(context, "第六章 可滚动组件", null);
+    return ListView.separated(
+      itemCount: _words.length,
+      itemBuilder: (context, index) {
+        //如果到了表尾
+        if (_words[index] == loadingTag) {
+          // 不足100条，继续获取数据
+          if (_words.length - 1 < 100) {
+            // 获取数据
+            _retrieveData();
+            // 加载时显示 loading
+            return Container(
+              padding: const EdgeInsets.all(16.0),
+              alignment: Alignment.center,
+              child: const SizedBox(
+                width: 24.0,
+                height: 24.0,
+                child: CircularProgressIndicator(strokeWidth: 2.0),
+              ),
+            );
+          } else {
+            //已经加载了100条数据，不再获取数据。
+            return Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(16.0),
+              child: const Text(
+                "没有更多了",
+                style: TextStyle(color: Colors.grey),
+              ),
+            );
+          }
+        }
+        //显示单词列表项
+        return ListTile(title: Text(_words[index]));
+      },
+      separatorBuilder: (context, index) => Divider(height: .0),
+    );
   }
 
+  void _retrieveData() {
+    Future.delayed(const Duration(seconds: 2)).then((e) {
+      setState(() {
+        //重新构建列表
+        _words.insertAll(
+          _words.length - 1,
+          // 每次生成20个单词
+          generateWordPairs().take(20).map((e) => e.asPascalCase).toList(),
+        );
+      });
+    });
+  }
 }
